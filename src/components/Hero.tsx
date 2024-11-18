ba947e8bbed4f9bec08d8abd9b356815b88b36c2
Esral Desta <esraldesta@gmail.com>
2024-11-18 11:34:30 +0300

    home

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -112,7 +112,7 @@ export default function Hero() {
 
 export const Header = () => {
     return (
-        <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
+        <div className="max-w-7xl relative mx-auto py-20 md:py px-4 w-full  left-0 top-0">
             <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
                 Alternative for <br /> Google forms
             </h1>
